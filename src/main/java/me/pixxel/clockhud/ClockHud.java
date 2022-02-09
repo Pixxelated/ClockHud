42c89396a0d79583f384d4666046e7da75c66670
Pixxelツ <2102sarthak@gmail.com>
2022-02-09 23:57:15 +0530

    initial commit

---
+++ src/main/java/me/pixxel/clockhud/ClockHud.java
@@ -0,0 +1,10 @@
+package me.pixxel.clockhud;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ClockHud implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/pixxel/clockhud/client/ClockHudClient.java
@@ -0,0 +1,13 @@
+package me.pixxel.clockhud.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ClockHudClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
